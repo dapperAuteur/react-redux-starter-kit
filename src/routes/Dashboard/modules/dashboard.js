ee99d755f8b01fa60dfe9bec21ace4883b9643bf
Anthony McDonald <info@elementarymba.com>
2016-12-10 21:43:35 -0700

    duplicate all counter directories, rename dashboard and change all references to counter in new dashboard directory to dashboard, add link to dashboard to index file

---
+++ src/routes/Dashboard/modules/dashboard.js
@@ -0,0 +1,55 @@
+// ------------------------------------
+// Constants
+// ------------------------------------
+export const DASHBOARD_VISITS_COUNT = 'DASHBOARD_VISITS_COUNT'
+
+// ------------------------------------
+// Actions
+// ------------------------------------
+export function increment (value = 1) {
+  return {
+    type: DASHBOARD_VISITS_COUNT,
+    payload: value
+  }
+}
+
+/*  This is a thunk, meaning it is a function that immediately
+    returns a function for lazy evaluation. It is incredibly useful for
+    creating async actions, especially when combined with redux-thunk!
+
+    NOTE: This is solely for demonstration purposes. In a real application,
+    you'd probably want to dispatch an action of COUNTER_DOUBLE and let the
+    reducer take care of this logic.  */
+
+export const doubleAsync = () => {
+  return (dispatch, getState) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        dispatch(increment(getState().dashboard))
+        resolve()
+      }, 200)
+    })
+  }
+}
+
+export const actions = {
+  increment,
+  doubleAsync
+}
+
+// ------------------------------------
+// Action Handlers
+// ------------------------------------
+const ACTION_HANDLERS = {
+  [DASHBOARD_VISITS_COUNT]: (state, action) => state + action.payload
+}
+
+// ------------------------------------
+// Reducer
+// ------------------------------------
+const initialState = 0
+export default function dashboardReducer (state = initialState, action) {
+  const handler = ACTION_HANDLERS[action.type]
+
+  return handler ? handler(state, action) : state
+}
