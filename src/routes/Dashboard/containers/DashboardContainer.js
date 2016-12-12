b89388b0ddcf5b1bea459cf1c5abbc419f8a548a
Anthony McDonald <info@elementarymba.com>
2016-12-11 19:41:28 -0700

    add new functions to DashboardContainer.js: inputOnChange, onSubmit, and itemOnEdit

--- src/routes/Dashboard/containers/DashboardContainer.js
+++ src/routes/Dashboard/containers/DashboardContainer.js
@@ -27,18 +27,70 @@ const mapStateToProps = (state) => ({
   dashboard: state.dashboard
 })
 
+// class DashboardContainer extends React.Component {
+//   componentDidMount() {
+//     this.props.dashboardVisitIncrement();
+//   }
+
+//   render () {
+//     return (
+//       <Dashboard {...this.props} />
+//     );
+//   }
+// }
 class DashboardContainer extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.inputOnChange = this.inputOnChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+    this.itemOnEdit = this.itemOnEdit.bind(this)
+
+    this.state ={
+      inputValue: '',
+      editedItemIndex: null
+    }
+  }
+
   componentDidMount() {
     this.props.dashboardVisitIncrement();
   }
 
+  inputOnChange(e) {
+    this.setState({ inputValue: e.target.value })
+  }
+
+  itemOnEdit(itemIndex) {
+    const editedItem = this.props.dashboard.dashboardItems[itemIndex]
+    this.setState({ inputValue: editedItem.label, editedItemIndex: itemIndex })
+  }
+
+  onSubmit(e) {
+    e.preventDefault()
+    const val = this.state.inputValue
+    const editedItemIndex = this.state.editedItemIndex
+    if(val && editedItemIndex !== null) {
+      this.props.dashboardEditItem({ val, editedItemIndex })
+      this.setState({ inputValue: '', editedItemIndex: null })
+    } else if(val) {
+      this.props.dashboardAddItem(val)
+      this.setState({ inputValue: '' })
+    } else {
+      alert(`Value can't be empty`)
+    }
+  }
+
   render () {
     return (
-      <Dashboard {...this.props} />
+      <Dashboard {...this.props}
+        editedItemIndex={this.state.editedItemIndex}
+        itemOnEdit={this.itemOnEdit}
+        inputValue={this.state.inputValue}
+        inputOnChange={this.inputOnChange}
+        onSubmit={this.onSubmit} />
     );
   }
 }
-
 /*  Note: mapStateToProps is where you should use `reselect` to create selectors, ie:
 
     import { createSelector } from 'reselect'
